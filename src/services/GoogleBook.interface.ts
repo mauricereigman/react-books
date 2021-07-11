d7b05e2e3667123db53ff60909f3c3aa76973256
Maurice Reigman <maurice.reigman@gmail.com>
2021-07-11 16:14:23 +0200

    fix: copy over missing dependencies from redux_implementation branch

--- src/services/GoogleBook.interface.ts
+++ src/services/GoogleBook.interface.ts
@@ -1,69 +1,118 @@
-export interface GoogleBook {
-    kind: string;
-    id: string;
-    etag: string;
-    selfLink: string;
-    volumeInfo: VolumeInfo;
-    saleInfo: SaleInfo;
-    accessInfo: AccessInfo;
+// @ts-nocheck
+import {IsBoolean, IsInt, IsString} from "class-validator";
+
+export class GoogleBook {
+    @IsString()
+    public readonly kind: string;
+    @IsString()
+    public readonly id: string;
+    @IsString()
+    public readonly etag: string;
+    @IsString()
+    public readonly selfLink: string;
+    @IsObject()
+    public readonly volumeInfo: VolumeInfo;
+    @IsObject()
+    public readonly saleInfo: SaleInfo;
+    @IsObject()
+    public readonly accessInfo: AccessInfo;
 }
 
-interface AccessInfo {
-    country: string;
-    viewability: string;
-    embeddable: boolean;
-    publicDomain: boolean;
-    textToSpeechPermission: string;
-    epub: Epub;
-    pdf: Epub;
-    webReaderLink: string;
-    accessViewStatus: string;
-    quoteSharingAllowed: boolean;
+class AccessInfo {
+    @IsString()
+    public readonly country: string;
+    @IsString()
+    public readonly viewability: string;
+    @IsBoolean()
+    public readonly embeddable: boolean;
+    @IsBoolean()
+    public readonly publicDomain: boolean;
+    @IsString()
+    public readonly textToSpeechPermission: string;
+    @IsObject()
+    public readonly epub: Epub;
+    @IsObject()
+    public readonly pdf: Epub;
+    @IsString()
+    public readonly webReaderLink: string;
+    @IsString()
+    public readonly accessViewStatus: string;
+    @IsBoolean()
+    public readonly quoteSharingAllowed: boolean;
 }
 
-interface Epub {
-    isAvailable: boolean;
+class Epub {
+    @IsBoolean()
+    public readonly isAvailable: boolean;
 }
 
-interface SaleInfo {
-    country: string;
-    saleability: string;
-    isEbook: boolean;
+class SaleInfo {
+    @IsString()
+    public readonly country: string;
+    @IsString()
+    public readonly saleability: stri
+    @IsBoolean()
+    public readonly isEbook: boolean;
 }
 
-interface VolumeInfo {
-    title: string;
-    authors: string[];
-    publishedDate: string;
-    description: string;
-    industryIdentifiers: IndustryIdentifier[];
-    readingModes: ReadingModes;
-    pageCount: number;
-    printType: string;
-    categories: string[];
-    averageRating: number;
-    ratingsCount: number;
-    maturityRating: string;
-    allowAnonLogging: boolean;
-    contentVersion: string;
-    imageLinks: ImageLinks;
-    language: string;
-    previewLink: string;
-    infoLink: string;
-    canonicalVolumeLink: string;
+class VolumeInfo {
+    @IsString()
+    public readonly title: string;
+    @IsObject()
+    public readonly authors: string[];
+    @IsString()
+    public readonly publishedDate: string;
+    @IsString()
+    public readonly description: string;
+    @IsObject()
+    public readonly industryIdentifiers: IndustryIdentifier[];
+    @IsObject()
+    public readonly readingModes: ReadingModes;
+    @IsInt()
+    public readonly pageCount: number;
+    @IsString()
+    public readonly printType: string;
+    @IsObject()
+    public readonly categories: string[];
+    @IsInt()
+    public readonly averageRating: number;
+    @IsInt()
+    public readonly ratingsCount: number;
+    @IsString()
+    public readonly maturityRating: string;
+    @IsBoolean()
+    public readonly allowAnonLogging: boolean;
+    @IsString()
+    public readonly contentVersion: string;
+    @IsObject()
+    public readonly imageLinks: ImageLinks;
+    @IsString()
+    public readonly language: string;
+    @IsString()
+    public readonly previewLink: string;
+    @IsString()
+    public readonly infoLink: string;
+    @IsString()
+    public readonly canonicalVolumeLink: string;
 }
 
-interface ImageLinks {
-    smallThumbnail: string;
-    thumbnail: string;
+class ImageLinks {
+    @IsString()
+    public readonly smallThumbnail: string;
+    @IsString()
+    public readonly thumbnail: string;
 }
 
-interface IndustryIdentifier {
-    type: string;
-    identifier: string;
+class IndustryIdentifier {
+    @IsString()
+    public readonly type: string;
+    @IsString()
+    public readonly identifier: string;
 }
 
-interface ReadingModes {
-    text: boolean;
-    image: boolean;
+class ReadingModes {
+    @IsBoolean()
+    public readonly text: boolean;
+    @IsBoolean()
+    public readonly image: boolean;
 }
