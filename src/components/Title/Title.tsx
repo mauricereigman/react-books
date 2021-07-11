749b2c5966eb90e62f7f1a5c01acce7ed1abc930
Maurice Reigman <maurice.reigman@gmail.com>
2021-07-11 15:20:21 +0200

    cleanup

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,18 +3,10 @@ import styles from './Title.module.scss';
 import {TitleProps} from "./TitleProps";
 
 const Title: FC<TitleProps> = (props) => {
-
-    const showSubtitle = () => {
-        if (props.subTitle) {
-            return (<h2>props.subTitle</h2>)
-        } else return null
-    }
-
     return (
         <div className={styles.Title} data-testid="Title">
             <h1>{props.title}</h1>
             {props.subTitle ? (<h2>{props.subTitle}</h2>) : null}
         </div>)
-
 }
 export default Title;
--- src/state/books/Slice.ts
+++ src/state/books/Slice.ts
@@ -44,7 +44,7 @@ export const booksSlice = createSlice({
     },
 })
 
-export const {} = booksSlice.actions
+// export const {} = booksSlice.actions
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
