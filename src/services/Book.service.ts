9b7eb92f54e76fcc5f67b367bfdb7b8b7c0945e6
Maurice Reigman <maurice.reigman@gmail.com>
2021-07-11 18:29:08 +0200

    feat: implement IO validation for google books api

--- src/services/Book.service.ts
+++ src/services/Book.service.ts
@@ -1,17 +1,49 @@
 import {Book} from "../models/Book";
-import {GoogleBook} from "./GoogleBook.interface";
+import {GoogleBook} from "./GoogleBook";
+import {validateOrReject} from "class-validator";
+import {Simulate} from "react-dom/test-utils";
+
 
 export class BooksService {
+    private static readonly googleBookValidationErrorMessage = "Validation error on google books api response"
     private static readonly googleBooksApi = "https://www.googleapis.com/books/v1";
 
-    public getBooks(searchQuery: string): Promise<Book[]> {
-        return fetch(`${BooksService.googleBooksApi}/volumes?q=${searchQuery}`)
-            .then(res => res.json())
-            .then(json => (json.items || []) as GoogleBook[])
-            .then(googleBooks => googleBooks.map(BooksService.toAppModel))
+    private static googleBooksAreValid(json: any): Promise<GoogleBook[]> {
+        const items = json.items
+        if (!items) {
+            console.error(BooksService.googleBookValidationErrorMessage)
+            throw new Error(BooksService.googleBookValidationErrorMessage)
+        }
+
+        return Promise.all(items.map((item: any) => BooksService.isGoogleBookValid(item)))
+    }
+
+    private static isGoogleBookValid(json: any): Promise<GoogleBook> {
+        const googleBookWithValidations = Object.assign(new GoogleBook(), json)
+        return validateOrReject(googleBookWithValidations, {forbidUnknownValues: true})
+            .then(() => googleBookWithValidations)
+            .catch((errors: any) => {
+                const error = `${BooksService.googleBookValidationErrorMessage}. Errors:, ${errors}`
+                console.error(error)
+                throw new Error(error)
+            })
     }
 
     private static toAppModel(googleBook: GoogleBook): Book {
         return new Book(googleBook.id, googleBook.volumeInfo.title, googleBook.volumeInfo.publishedDate, googleBook.volumeInfo.imageLinks.thumbnail, googleBook.volumeInfo.authors)
+    }
+
+    public getBooks(searchQuery: string): Promise<Book[]> {
+        return fetch(`${BooksService.googleBooksApi}/volumes?q=${searchQuery}`)
+            .then(res => res.json())
+            .then(BooksService.googleBooksAreValid)
+            .then(googleBooks => googleBooks.map(BooksService.toAppModel))
+    }
+
+    public getBook(id: string): Promise<Book> {
+        return fetch(`${BooksService.googleBooksApi}/volumes/${id}`)
+            .then(res => res.json())
+            .then(googleBook => BooksService.isGoogleBookValid(googleBook))
+            .then(BooksService.toAppModel)
     }
 }
---
+++ src/services/GoogleBook.ts
@@ -0,0 +1,116 @@
+// @ts-nocheck
+import {IsBoolean, IsInt, IsObject, IsString} from "class-validator";
+
+export class GoogleBook {
+    @IsString()
+    public readonly kind: string;
+    @IsString()
+    public readonly id: string;
+    @IsString()
+    public readonly etag: string;
+    @IsString()
+    public readonly selfLink: string;
+    @IsObject()
+    public readonly volumeInfo: VolumeInfo;
+    @IsObject()
+    public readonly saleInfo: SaleInfo;
+    @IsObject()
+    public readonly accessInfo: AccessInfo;
+}
+
+class AccessInfo {
+    @IsString()
+    public readonly country: string;
+    @IsString()
+    public readonly viewability: string;
+    @IsBoolean()
+    public readonly embeddable: boolean;
+    @IsBoolean()
+    public readonly publicDomain: boolean;
+    @IsString()
+    public readonly textToSpeechPermission: string;
+    @IsObject()
+    public readonly epub: Epub;
+    @IsObject()
+    public readonly pdf: Epub;
+    @IsString()
+    public readonly webReaderLink: string;
+    @IsString()
+    public readonly accessViewStatus: string;
+    @IsBoolean()
+    public readonly quoteSharingAllowed: boolean;
+}
+
+class Epub {
+    @IsBoolean()
+    public readonly isAvailable: boolean;
+}
+
+class SaleInfo {
+    @IsString()
+    public readonly country: string;
+    @IsString()
+    public readonly saleability: stri
+    @IsBoolean()
+    public readonly isEbook: boolean;
+}
+
+class VolumeInfo {
+    @IsString()
+    public readonly title: string;
+    @IsObject()
+    public readonly authors: string[];
+    @IsString()
+    public readonly publishedDate: string;
+    @IsString()
+    public readonly description: string;
+    @IsObject()
+    public readonly industryIdentifiers: IndustryIdentifier[];
+    @IsObject()
+    public readonly readingModes: ReadingModes;
+    @IsInt()
+    public readonly pageCount: number;
+    @IsString()
+    public readonly printType: string;
+    @IsObject()
+    public readonly categories: string[];
+    @IsInt()
+    public readonly averageRating: number;
+    @IsInt()
+    public readonly ratingsCount: number;
+    @IsString()
+    public readonly maturityRating: string;
+    @IsBoolean()
+    public readonly allowAnonLogging: boolean;
+    @IsString()
+    public readonly contentVersion: string;
+    @IsObject()
+    public readonly imageLinks: ImageLinks;
+    @IsString()
+    public readonly language: string;
+    @IsString()
+    public readonly previewLink: string;
+    @IsString()
+    public readonly infoLink: string;
+    @IsString()
+    public readonly canonicalVolumeLink: string;
+}
+
+class ImageLinks {
+    @IsString()
+    public readonly smallThumbnail: string;
+    @IsString()
+    public readonly thumbnail: string;
+}
+
+class IndustryIdentifier {
+    @IsString()
+    public readonly type: string;
+    @IsString()
+    public readonly identifier: string;
+}
+
+interface ReadingModes {
+    text: boolean;
+    image: boolean;
+}
